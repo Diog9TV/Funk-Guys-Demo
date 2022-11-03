103f16db125d049afd7b9b44e1fcaf6836d88789
Diog9TV <89103529+Diog9TV@users.noreply.github.com>
2022-11-03 08:46:50 -0300

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1158,6 +1158,13 @@ class PlayState extends MusicBeatState
 		botplayTxt.borderSize = 1.25;
 		botplayTxt.visible = cpuControlled;
 		add(botplayTxt);
+		
+    var creditTxt = new FlxText(876, 648, 348);
+  creditTxt.text = "Ported By Ninja";
+  creditTxt.setFormat(Paths.font("vcr.ttf"), 30, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
+  creditTxt.scrollFactor.set();
+   add(creditTxt);
+
 		if(ClientPrefs.downScroll) {
 			botplayTxt.y = timeBarBG.y - 78;
 		}
@@ -1166,6 +1173,7 @@ class PlayState extends MusicBeatState
 		grpNoteSplashes.cameras = [camHUD];
 		notes.cameras = [camHUD];
 		healthBar.cameras = [camHUD];
+		creditTxt.cameras = [camHUD];
 		healthBarBG.cameras = [camHUD];
 		iconP1.cameras = [camHUD];
 		iconP2.cameras = [camHUD];
